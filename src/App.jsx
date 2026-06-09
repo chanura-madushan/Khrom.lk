a0852189e00499580bf6c9d5068a16cfe11bf669
chanura-madushan <chanuramadushan18@gmail.com>
2026-06-09 21:43:48 +0530

    code_readability-1

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import Home from "./HomeUI/Home.jsx";
-import Signin from "./HomeUI/Signin.jsx";
+import Home from "./HomeUI/Jsx Files/Home.jsx";
+import Signin from "./HomeUI/Jsx Files/Signin.jsx";
 
 function App() {
   return (

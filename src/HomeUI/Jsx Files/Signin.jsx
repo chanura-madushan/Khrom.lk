2b6e5c51da0b07580bb22dec368bd0a798626149
chanura-madushan <chanuramadushan18@gmail.com>
2026-06-10 19:51:12 +0530

    code-readability-improved

--- src/HomeUI/Jsx Files/Signin.jsx
+++ src/HomeUI/Jsx Files/Signin.jsx
@@ -47,7 +47,7 @@ function Signin() {
         </div>
       </div>
 
-      {/* Right Side: Sign In Form */}
+     
       <div className="signin-form-section">
         <div className="form-wrapper">
           <h2 className="form-title">Create an account</h2>
@@ -91,7 +91,7 @@ function Signin() {
               <input
                 type={showPassword ? "text" : "password"}
                 name="password"
-                placeholder="Enter your passwrod" /* Kept exact typo from UI image */
+                placeholder="Enter your passwrod" 
                 value={formData.password}
                 onChange={handleChange}
               />
@@ -100,7 +100,7 @@ function Signin() {
                 className="password-toggle"
                 onClick={() => setShowPassword(!showPassword)}
               >
-                {/* SVG Eye Icon */}
+                {}
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                   <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
                   <circle cx="12" cy="12" r="3"></circle>
@@ -132,7 +132,7 @@ function Signin() {
 
           <div className="social-login-row">
             <button className="social-btn">
-              {/* Google SVG Icon */}
+              {}
               <svg className="social-icon" viewBox="0 0 24 24" width="18" height="18" xmlns="http://www.w3.org/2000/svg">
                 <path d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z" fill="#4285F4"/>
                 <path d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z" fill="#34A853"/>
